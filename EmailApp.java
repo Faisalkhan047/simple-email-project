ab70b9c8be459d318f75751dc714eee6bf282e12
Faisalkhan047 <97181889+Faisalkhan047@users.noreply.github.com>
2023-03-23 14:49:54 +0530

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,24 @@
+package com.emailApp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+	  
+		email e1=new email();
+		/*  
+		 create objects and use the following methods
+		  
+		  1.changePasword
+		  2.setMailCapacity
+		  3.getMailCapacity
+		  4.changePassword
+		  5.SetAlternateEmail
+		  6.getAlternateEmail
+		  7.showInfo
+		
+		 */
+		e1.ShowInfo();
+		
+	}
+
+}
---
+++ email.java
@@ -0,0 +1,184 @@
+package com.emailApp;
+
+import java.util.Scanner;
+
+public class email {
+	  
+	//all variable that can be used
+	
+      private String FirstName;
+      private String LastName;
+      private String DefaultPassword;
+      private String newPassword;
+      private String Department;
+      private String email;
+      private String alternateEmail;
+      private String userInfo;  
+      private int MailBoxCapacity;
+      private String OTP;
+      
+      Scanner sc=new Scanner(System.in);
+      
+      email(){
+    	 
+    	  //calling the respective functions to set first name last name and department 
+    	  
+    	   this.FirstName=this.setFirstName();
+    	   this.LastName=this.setLastName();
+    	  this.Department=this.setDepartment();
+    	  
+    	  //calling the setOTP method for generating an otp 
+    	  
+    	  this.OTP=this.setOTP(6);
+    	  System.out.println("Enter this OTP to get proceed further:"+this.OTP);
+    	  System.out.println("Enter the Otp:");
+    	 String validotp=sc.next();
+    	  
+    	 
+    	 //otp validation
+    	 
+    	 if(validotp.equals(this.OTP)) {
+    		 
+    		 //format for proper email i.e firstnamelastname@department.com
+    		 
+    	 email=this.FirstName.toLowerCase()+this.LastName.toLowerCase()+"@"+this.Department.toLowerCase()+".com";
+    	  System.out.println("Your email looks like this..."+this.email);
+    	  
+    	  //calling RandomPassword method to generate default password
+    	  
+    	  this.DefaultPassword=this.setRandomPassword(6);
+    	  System.out.println("Generating a Default Password for you:"+this.DefaultPassword);
+
+    	  
+    	  
+    	  //calling mail capacity method set mailbox capacity
+    	  
+    	  this.MailBoxCapacity=this.setMailCapacity();
+    	  
+          this.alternateEmail=this.setAlternateEmail();
+//          System.out.println("Your alternate email is"+this.alternateEmail);
+          
+          this.newPassword=this.setPassword();
+//          System.out.println("Your new password is:"+this.newPassword);
+          
+    	  this.userInfo=this.ShowInfo();
+    	  System.out.println("================= USER DETAILS =============");
+    	  System.out.println(userInfo);
+    	  
+    	  
+    	  
+    	 }
+    	  else {System.out.println("Wrong OTP..");
+    	 }
+    	  
+    	 }
+    	 
+    	  
+    	 
+      
+      
+      
+      //Asking for first name
+      
+      private String setFirstName() {
+    	  System.out.println("Enter your first name :");
+    	  String fname=sc.nextLine();
+    	  this.FirstName=fname;
+    	  return this.FirstName;
+      }
+      
+      //Asking for the last name
+      
+      private String setLastName() {
+    	  System.out.println("Enter YOur last name:");
+    	  String Lname=sc.nextLine();
+    	  this.LastName=Lname;
+    	  return this.LastName;
+      }
+      
+      
+      //setting the department for the email
+      
+      private String setDepartment() {
+    	  System.out.println("Enter a choice for the Department:\n 1.Sales \n 2.HR \n 3.Development \n 4.Accounting \n 5.None/Others");
+    	  Scanner sc=new Scanner(System.in);
+    	  int n=sc.nextInt();
+    	  if(n==1) {return "Sales";}
+    	  else if(n==2) {return "HR";}
+    	  else if(n==3) {return "Development";}
+    	  else if(n==4) {return "Accounting";}
+    	  else if(n==5) {return "None/Other";}
+		return Department;
+    	  
+      }
+      
+      //Generating a random/default password for the user 
+      
+      private String setRandomPassword(int length) {
+    	  String passwordLength="ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*abcdefghijklmnopqrstuvwxyz";
+    	  char[] password=new char[length];
+    	  for(int i=0; i<length;i++) {
+    		  int rand=(int)(Math.random()* passwordLength.length());
+    		  password[i]=passwordLength.charAt(rand);
+    	  }
+    	  return new String(password);
+      }
+      
+      
+      // Ask for the mailbox capacity i.e no.of mails to be stored
+      
+      public int setMailCapacity() {
+    	  System.out.println("Enter the capacity you want for your mail box:");
+    	  int capacity=sc.nextInt();
+    	  this.MailBoxCapacity=capacity;
+    	  return this.MailBoxCapacity;
+      }
+      
+      
+      
+      // Ask an alternate email from user only changing first name
+      
+      public String setAlternateEmail() {
+    	  System.out.println("enter your Alternate email(Only first name):");
+    	  
+    	  String anotherEmail=sc.next();
+    	  this.alternateEmail=anotherEmail;
+    	  return this.alternateEmail;
+      }
+      
+      
+      
+      // Generating the user defined password
+      
+      public String setPassword() {
+    	  System.out.println("Enter the password you want to change:");
+    	 String nPassword=sc.next();
+    	  this.newPassword=nPassword;
+    	  return newPassword;
+      }
+      
+      
+      
+      
+      // Generating OTP
+      
+      public String setOTP(int length) {
+    	  String OTPlength="1234567890";
+    	  char[] OTP=new char[length];
+    	  for(int i=0; i<length; i++) {
+    		  int rand=(int)(Math.random()*OTPlength.length());
+    		  OTP[i]=OTPlength.charAt(rand);
+    	  }
+    	  return new String(OTP);
+      }
+      
+      
+      // All user Information
+      
+      public String ShowInfo() {
+    	  return "\n Firstname:"+this.FirstName+"\n Department:"+this.Department+"\n Email capacity:"
+                    +this.MailBoxCapacity+"\n Your default password was:"+this.DefaultPassword+"\n Your new pass:"
+    			  +this.newPassword+"\nYour alternate email is:"+this.alternateEmail.toLowerCase()+this.LastName.toLowerCase()+"@"+this.Department.toLowerCase()+".com"+"\n Your main email is:"+this.email;
+      }
+      
+}
