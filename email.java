ac27cddfaaf25f2aa77095399a8ad7227fb6897f
Faisalkkhan047 <mdfaisalkhan436@gmail.com>
2023-08-28 00:07:38 +0530

    change password feature updated

--- email.java
+++ email.java
@@ -16,62 +16,103 @@ public class email {
       private String userInfo;  
       private int MailBoxCapacity;
       private String OTP;
+
       
       Scanner sc=new Scanner(System.in);
       
       email(){
-    	 
+    	  for(int i=0; i<100; i++) {
+    	  System.out.println("\n1.Create email \n2.change password \n3.change mailbox capacity");
+    	  
+
+    	   int choice=sc.nextInt();
+    	   
+    	   if(choice==1) {
+    		   this.FirstName=this.setFirstName();
+        	   this.LastName=this.setLastName();
+        	  this.Department=this.setDepartment();
+        	  
+        	  this.OTP=this.setOTP(6);
+        	  System.out.println("Enter this OTP to get proceed further:"+this.OTP);
+        	  System.out.println("Enter the Otp:");
+        	 String validotp=sc.next();
+        	 
+        	 if(validotp.equals(this.OTP)) {
+        		 
+        		 //format for proper email i.e firstnamelastname@department.com
+        		 
+        	 email=this.FirstName.toLowerCase()+this.LastName.toLowerCase()+"@"+this.Department.toLowerCase()+".com";
+        	  System.out.println("Your email looks like this..."+this.email);
+        	  
+        	  
+        	  this.DefaultPassword=this.setRandomPassword(6);
+        	  System.out.println("Generating a Default Password for you:"+this.DefaultPassword);
+        	  
+        	  this.MailBoxCapacity=this.setMailCapacity();
+        	  
+        	  
+        	  
+        	  
+        	   this.alternateEmail=this.setAlternateEmail();
+//             System.out.println("Your alternate email is"+this.alternateEmail);
+             
+             this.newPassword=this.setPassword();
+//             System.out.println("Your new password is:"+this.newPassword);
+             
+       	  this.userInfo=this.ShowInfo();
+       	  System.out.println("================= USER DETAILS =============");
+       	  System.out.println(userInfo);
+       	  System.out.println("=============================================");
+       	  
+       	  
+       	  
+       	 }
+       	  else {System.out.println("Wrong OTP..");
+       	 }}
+       	  
+        	 else if(choice==2) {
+        		 System.out.println("Enter old password:");
+        		 String oldPassword=sc.next();
+        		 if(oldPassword==this.newPassword) {
+        			 System.out.println("Enter new Password:");
+        			 String modifiedPassword=sc.next();
+        			 this.newPassword=modifiedPassword;
+        			 System.out.println("Password changed as:"+this.newPassword);
+        			 
+        		 }
+        		 
+        	 }
+        	 
+        	 else if(choice==3) {
+        		 System.out.println("setting the mail box capacity:");
+        	 }
+
+        	  
+    	   }
+      }
     	  //calling the respective functions to set first name last name and department 
     	  
-    	   this.FirstName=this.setFirstName();
-    	   this.LastName=this.setLastName();
-    	  this.Department=this.setDepartment();
+    	   
     	  
     	  //calling the setOTP method for generating an otp 
     	  
-    	  this.OTP=this.setOTP(6);
-    	  System.out.println("Enter this OTP to get proceed further:"+this.OTP);
-    	  System.out.println("Enter the Otp:");
-    	 String validotp=sc.next();
+    	 
     	  
     	 
     	 //otp validation
     	 
-    	 if(validotp.equals(this.OTP)) {
-    		 
-    		 //format for proper email i.e firstnamelastname@department.com
-    		 
-    	 email=this.FirstName.toLowerCase()+this.LastName.toLowerCase()+"@"+this.Department.toLowerCase()+".com";
-    	  System.out.println("Your email looks like this..."+this.email);
-    	  
+    	 
     	  //calling RandomPassword method to generate default password
     	  
-    	  this.DefaultPassword=this.setRandomPassword(6);
-    	  System.out.println("Generating a Default Password for you:"+this.DefaultPassword);
-
+    	  
     	  
     	  
     	  //calling mail capacity method set mailbox capacity
     	  
-    	  this.MailBoxCapacity=this.setMailCapacity();
+    	 
     	  
-          this.alternateEmail=this.setAlternateEmail();
-//          System.out.println("Your alternate email is"+this.alternateEmail);
-          
-          this.newPassword=this.setPassword();
-//          System.out.println("Your new password is:"+this.newPassword);
-          
-    	  this.userInfo=this.ShowInfo();
-    	  System.out.println("================= USER DETAILS =============");
-    	  System.out.println(userInfo);
-    	  
-    	  
-    	  
-    	 }
-    	  else {System.out.println("Wrong OTP..");
-    	 }
-    	  
-    	 }
+       
+    	 
     	 
     	  
     	 
@@ -82,7 +123,7 @@ public class email {
       
       private String setFirstName() {
     	  System.out.println("Enter your first name :");
-    	  String fname=sc.nextLine();
+    	  String fname=sc.next();
     	  this.FirstName=fname;
     	  return this.FirstName;
       }
@@ -91,7 +132,7 @@ public class email {
       
       private String setLastName() {
     	  System.out.println("Enter YOur last name:");
-    	  String Lname=sc.nextLine();
+    	  String Lname=sc.next();
     	  this.LastName=Lname;
     	  return this.LastName;
       }
@@ -101,7 +142,7 @@ public class email {
       
       private String setDepartment() {
     	  System.out.println("Enter a choice for the Department:\n 1.Sales \n 2.HR \n 3.Development \n 4.Accounting \n 5.None/Others");
-    	  Scanner sc=new Scanner(System.in);
+//    	  Scanner sc=new Scanner(System.in);
     	  int n=sc.nextInt();
     	  if(n==1) {return "Sales";}
     	  else if(n==2) {return "HR";}
